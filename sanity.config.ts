fe3ef8d2b66efcbf02ad7b23ac6ce929d0303064
Noah Gentile <noah.gentile@gmail.com>
2025-03-06 16:55:27 -0800

    adjust tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,35 +1,15 @@
-import {codeInput} from '@sanity/code-input'
-import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {internationalizedArray} from 'sanity-plugin-internationalized-array'
 
 import {attributes} from './plugins/attributes'
-import {LANGUAGES} from './src/lib/languages'
-import {schemaTypes} from './src/schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'Attribute Limit',
 
-  projectId: 'pjpqerlt',
-  dataset: 'attribute-example',
+  projectId: 'h7diz1ml',
+  dataset: 'dev',
 
-  plugins: [
-    structureTool(),
-    visionTool(),
-    internationalizedArray({
-      languages: LANGUAGES,
-      defaultLanguages: ['en'],
-      fieldTypes: ['string', 'body'],
-    }),
-    attributes(),
-    codeInput(),
-  ],
-
-  schema: {
-    types: schemaTypes,
-  },
+  plugins: [attributes()],
 
   scheduledPublishing: {
     enabled: false,
